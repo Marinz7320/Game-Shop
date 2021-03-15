059476e1d4441365846c2e7cf3f767816d2d77b1
hrvojezovko <hrvoje.zovko10@hotmail.com>
2021-03-15 21:14:44 +0100

    last commit

---
+++ game-shop-frontend/src/data_psn.js
@@ -0,0 +1,184 @@
+import Game16 from './components/img/Game16.png'
+import Game17 from './components/img/Game17.png'
+import Game18 from './components/img/Game18.png'
+import Game19 from './components/img/Game19.png'
+import Game20 from './components/img/Game20.png'
+import Game21 from './components/img/Game21.png'
+import Game22 from './components/img/Game22.png'
+import Game23 from './components/img/Game23.png'
+import Game24 from './components/img/Game24.png'
+import Game25 from './components/img/Game25.png'
+import Game26 from './components/img/Game26.png'
+import Game27 from './components/img/Game27.png'
+import Game28 from './components/img/Game28.png'
+import Game29 from './components/img/Game29.png'
+import Game30 from './components/img/Game30.png'
+
+
+export const psnn = [
+    {
+        _id: 16,
+        title: "Mortal Kombat 11 | Aftermath Kollection",
+        src: Game16,
+        content: "Mortal Kombat 11 retains many of the Mortal Kombat 10’s gameplay mechanics, including Fatalities and Brutalities, brutally graphic finishing moves. Each character has a specific Fatality, which references their personality in some way. Former action movie star Johnny Cage beats his opponents with a gilded figure of himself, gunslinger Erron Black uses his revolvers to riddle enemies with bullets, and so on.",
+        price: 29.86,
+        onsale: 59.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 17,
+        title: "EA SPORTS FIFA 21",
+        src: Game17,
+        content: "FIFA 21 is a football (soccer) sports game developed by EA Vancouver and published by EA Sports. The game is yet another installment of classic football simulation series, dating back to the late nineties of the last century. In FIFA21, the player will once again play as their favorite team, participating in matches with other players through online multiplayer or against the computer offline.",
+        price: 34.99,
+        onsale: 59.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 18,
+        title: "Mortal Kombat 11 | Ultimate",
+        src: Game18,
+        content: "Mortal Kombat 11 retains many of the Mortal Kombat 10’s gameplay mechanics, including Fatalities and Brutalities, brutally graphic finishing moves. Each character has a specific Fatality, which references their personality in some way. Former action movie star Johnny Cage beats his opponents with a gilded figure of himself, gunslinger Erron Black uses his revolvers to riddle enemies with bullets, and so on.",
+        price: 49.39,
+        onsale: 0,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 19,
+        title: "Sackboy: A Big Adventure",
+        src: Game19,
+        content: "Iconic PlayStation® hero Sackboy bursts back into breathtaking action with a huge, fun and frantic 3D multiplayer platforming adventure – and a whole new edgy sackitude! And here’s what’s going down: The dastardly Vex (a near-mythical being born of chaos and fear, no less) kidnaps Sackboy’s friends and forces them to build his Topsy Turver.",
+        price: 63.84,
+        onsale: 99.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 20,
+        title: "Concrete Genie",
+        src: Game20,
+        content: "Concrete Genie is a game about painting. You play as a young boy named Ash, a budding artist who is also subjected to a constant stream of bullying. ... While Ash is searching for his lost sketchbook pages, Luna also tasks him with bringing the dreary Denska to life through his paintings.",
+        price: 15.65,
+        onsale: 29.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 21,
+        title: "Predator: Hunting Grounds",
+        src: Game21,
+        content: "Predator: Hunting Grounds is an asymmetrical shooter set in the vast area where you become either a hunter or the prey. In the game, you can play as a member of a Fireteam and your job is to complete the objectives before the Predator hunts you down, or you can play as this mysterious alien hunter, a killing machine equipped with a deadly arsenal, whose main target is removing your squad from existence.",
+        price: 28.86,
+        onsale: 0,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 22,
+        title: "Star Wars Jedi: Fallen Order",
+        src: Game22,
+        content: "Star Wars Jedi: Fallen Order is a single player, third-person perspective action game, published by Electronic Arts and developed by Respawn Entertainment, the studio behind such titles as the Titanfall series and Apex: Legends. The game is set between Episodes III and IV of the Star Wars saga and features a brand new story and characters. The main protagonist of the game is Cal Kestis, young Jedi padawan, who survived the execution of Order 66 and has since been on the run from the Empire.",
+        price: 20.48,
+        onsale: 59.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 23,
+        title: "Tony Hawk's™ Pro Skater™",
+        src: Game23,
+        content: "Play the fully-remastered Tony Hawk’s™ Pro Skater™ & Tony Hawk’s™ Pro Skater™ 2 games in one epic collection, rebuilt from the ground up in incredible HD.Drop Back In With The Most Iconic Skateboarding Games Ever Made.Skate as the legendary Tony Hawk and the full pro roster. Listen to songs from the era-defining soundtrack. Hit insane trick combos with the iconic handling of the Tony Hawk’s™ Pro Skater™ series.All The Original Game Modes And More.",
+        price: 50.60,
+        onsale: 0,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 24,
+        title: "Battlefield V",
+        src: Game24,
+        content: "Focused heavily on going back to its roots, Battlefield V features a complex, diverse solo campaign letting you explore the storyline from different perspectives. Take control of several different protagonists and take part in some of the biggest battles in history. Participate in the story of a German woman fighting in The Resistance against the Nazi occupation. Uncover many other unique stories gathered in the War Stories mode.",
+        price: 46.33,
+        onsale: 69.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 25,
+        title: "Crash Bandicoot 4: It’s About Time",
+        src: Game25,
+        content: "It’s About Time - for a brand-wumping new Crash Bandicoot™ game! Crash fourward into a time shattered adventure with your favorite marsupials.Neo Cortex and N. Tropy are back at it again and launching an all-out assault on not just this universe, but the entire multiverse! Crash and Coco are here to save the day by reuniting the four Quantum Masks and bending the rules of reality.New abilities? Check. More playable characters? Yep.",
+        price: 69.67,
+        onsale: 0,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 26,
+        title: "Batman: Arkham Knight",
+        src: Game26,
+        content: "Batman: Arkham Knight is a third-person perspective action-adventure game, developed by Rocksteady Studios and published by Warner Bros. Interactive Entertainment. It is the fourth installment of the Arkham series and a culmination of the story begun in Batman: Arkham Asylum. AK features an open-world environment of Gotham City, vehicular gameplay using the Batmobile and several other new mechanics. The plot of the game will take the Bat on a ride to hell and back.",
+        price: 9.10,
+        onsale: 16.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 27,
+        title: "Port Royale 4",
+        src: Game27,
+        content: "Develop production chains connecting multiple islands and create complex trade routes across the Caribbean, covering the ever-growing needs of the respective cities. Make use of the detailed sea map to avoid stormy weather regions, cliffs or shallow waters. Fulfill tasks for your nation’s viceroy, earning more fame to unlock town buildings, ships and more. Conquer the cities of rival nations or hunt down their fleets with a letter of marque while also keeping a keen eye out for pirates and other privateers.",
+        price: 53.76,
+        onsale: 59.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 28,
+        title: "Call of Duty: WWII",
+        src: Game28,
+        content: "Call of Duty returns to its roots with Call of Duty: WWII - a breathtaking experience that redefines World War II for a new gaming generation. Land in Normandy on D-Day and battle across Europe through iconic locations in history’s most monumental war. Experience classic Call of Duty combat, the bonds of camaraderie, and the unforgiving nature of war against a global power throwing the world into tyranny.",
+        price: 66.88,
+        onsale: 0,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 29,
+        title: "Fast & Furious: Crossroads",
+        src: Game29,
+        content: "Fast & Furious Crossroads is an action-adventure video game set in the Fast & Furious universe. Gear up for fast, cinematic non-stop action in a new chapter of the Fast & Furious saga as you hunt down an international crime syndicate and pump the brakes on their plans. With adrenaline-fueled stunts and heists, gadgets, exotic locations, cars, and heroes from the films, Fast & Furious Crossroads casts you right at the center of the action.",
+        price: 59.99,
+        onsale: 0,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 30,
+        title: "Borderlands 3",
+        src: Game30,
+        content: "Borderlands 3 is a co-op first-person shooter game developed by Gearbox Software and published by 2K Games. It is the sequel of Borderlands 2 and the fourth installment in the Borderlands series. The game focuses on exploring open world environments of the titular Borderlands and killing enemies with an array of weapons. The game puts great emphasis on looting fallen adversaries and boxes scattered around the map for new tools of destruction, as well as gaining experience points and leveling up one of four playable characters with unique skill sets.",
+        price: 58.23,
+        onsale: 69.99,
+        new: "",
+        count: 1,
+        discount: 0
+    }
+]
---
+++ game-shop-frontend/src/data_xbox.js
@@ -0,0 +1,183 @@
+import Game31 from './components/img/Game31.png'
+import Game32 from './components/img/Game32.png'
+import Game33 from './components/img/Game33.png'
+import Game34 from './components/img/Game34.png'
+import Game35 from './components/img/Game35.png'
+import Game36 from './components/img/Game36.png'
+import Game37 from './components/img/Game37.png'
+import Game38 from './components/img/Game38.png'
+import Game39 from './components/img/Game39.png'
+import Game40 from './components/img/Game40.png'
+import Game41 from './components/img/Game41.png'
+import Game42 from './components/img/Game42.png'
+import Game43 from './components/img/Game43.png'
+import Game44 from './components/img/Game44.png'
+import Game45 from './components/img/Game45.png'
+
+export const xboxx = [
+    {
+        _id: 31,
+        title: "Forza Horizon 4",
+        src: Game31,
+        content: "Visit the beautiful places of Great Britain with the best cars in the world and encounter the real adventure. Forza Horizon 4 ( Xbox One / PC ) is an online racing game developed by Playground Games. It was revealed at E3 2018 and released on October 2, 2018. This is the fourth installment of the series Forza Horizon and the eleventh in the Forza Motorsport saga and will be set in the United Kingdom. Drive across beautiful locations and explore over 450 cars.",
+        price: 37.77,
+        onsale: 69.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 32,
+        title: "Assassin's Creed Unity",
+        src: Game32,
+        content: "Travel back in time to the French Revolution. Experience this tumultuous time as Arno, a freshly inducted assassin with a complicated childhood. Assassin's Creed Unity (Xbox One) brings the franchise back to Europe, to a gorgeously recreated Paris, specifically.A massive number of activities above and beyond core missions make Assassin's Creed Unity a game that will keep you engaged for hours.",
+        price: 29.99,
+        onsale: 0,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 33,
+        title: "Assassin's Creed: Valhalla",
+        src: Game33,
+        content: "Assassin’s Creed Valhalla is an open world action-adventure game developed by Ubisoft Montreal and published by Ubisoft in 2020. It’s the twelfth main game in the AC series, and a third to feature the new gameplay mechanics of Ac: Origins, a mesh of action and RPG elements. The game is set during the time of the Viking invasion of Britain. The player takes control of Eivor, a leader of a Viking clan, seeking refuge on the eastern shores of England.",
+        price: 39.99,
+        onsale: 59.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 34,
+        title: "EA Sports UFC 4",
+        src: Game34,
+        content: "The first UFC fighting game from the EA Sports UFC series was released in 2014 for PlayStation 4 and Xbox One. The series gained much popularity and the MMA-lovers were amazed by the complex Career mode with the possibility to create one’s own MMA fighter. The fights were very realistic and the game features full body deformations.",
+        price: 34.49,
+        onsale: 0,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 35,
+        title: "Forza Motorsport 7",
+        src: Game35,
+        content: "Developed since 2005, the Forza Motorsport series shows how to create best-selling titles acclaimed as some of the most realistic racing games in the world. Its constantly increasing group of fans cherishes the unmistakable atmosphere and authenticity, visible in everything from the scratch on your rear light, through the gearbox’ work, to the faithfully reflected racetracks. Everything originally licensed, everything prepared in accordance with experts’ guidance.",
+        price: 31.17,
+        onsale: 39.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 36,
+        title: "The Crew 2",
+        src: Game36,
+        content: "Take to the roads, to the skies, and to the sea in The Crew 2, a sequel to a unique racing game from 2014. Improving in every respect over its predecessor The Crew 2 gives you access to an astonishing number of vehicles of all kinds, including, for the first time, planes and boats.Are you going to prove that you are the fastest on land, water, and in the air? Ivory Tower's production will put that claim to the test.",
+        price: 15.65,
+        onsale: 69.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 37,
+        title: "Call of Duty Black Ops: Cold War",
+        src: Game37,
+        content: "Call of Duty: Black Ops Cold War is the next game of the Call of Duty series, created by the people working on CoD: BO since 2010, Treyarch studio, acting under the watchful eye of Activision Blizzard. The original Black Ops is by many considered the most exciting and thrilling installment of the whole CoD series, and the Cold War is the direct continuation of the history presented in the first BO. The video game created by Treyarch presents extremely high-quality visuals thanks to the advanced technology developed by the Infinity Ward.",
+        price: 53.43,
+        onsale: 59.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 38,
+        title: "Red Dead Redemption 2",
+        src: Game38,
+        content: "Red Dead Redemption 2 is an action-adventure game it was released on Xbox One and PlayStation 4, and since 2019 it is also available on the PC. The game is a prequel to the Red Dead Redemption released in 2010 and focuses on the life of the outlaw Arthur Morgan. Enter and experience a truly massive world with an astounding amount of activities, interactions, beautiful visuals and more. If you have ever played the first RDR game, you might find a number of references to the characters and events from this game.",
+        price: 29.99,
+        onsale: 0,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 39,
+        title: "Halo: The Master Chief Collection",
+        src: Game39,
+        content: "Halo: The Master Chief Collection is a must buy for first-person-shooter fans! Revision the superior versions of first four numbered Halo games. The Master Chief Collection contains Halo: Combat Evolved, Halo 2: Anniversary, campaigns and multiplayer maps from Halo 2 Vista, Halo 3 and Halo 4. If you buy this collection, you’ll also get access to Halo: Nightfall and Halo 5: Guardians Multiplayer Beta.",
+        price: 23.60,
+        onsale: 29.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 40,
+        title: "NHL 21 | Standard Edition",
+        src: Game40,
+        content: "NHL 21 is the new game of the world-famous hockey simulation series, developed by EA Vancouver. The developers once again tried to deliver immersive experiences, present during real-life NHL games. NHL 2021 is the 30th installment of the hockey video game series which beginnings date back to 1994. The game features a number of new mechanics and gameplay elements innovative for the NHL series. The gameplay is characterized by its realism, simulating the National Hockey League matches.",
+        price: 43.99,
+        onsale: 49.99,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 41,
+        title: "Cyberpunk 2077",
+        src: Game41,
+        content: "Cyberpunk 2077 gameplay allows you to mix and match your approaches to suit your playstyle. You can go in guns blazing, or sneak around your enemies and eliminate them with your melee weapons or hacking vulnerable devices. Either way, combat is violent and satisfying. As a story-rich adventure, CP2077 lets you carve your own position among the shakers and movers of Night City, arrange your own alliances, and find your own motivations.",
+        price: 61.67,
+        onsale: 0,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 42,
+        title: "Tom Clancy's The Division 2",
+        src: Game42,
+        content: "Restore order and prevent the collapse of the Washington D.C. Play with your friends in coop and PvP, get powerful weapons & equipment and become one of the chosen ones to protect the Capitol in a state of emergency. The mission does not end with the campaign. Beyond level 30, the Division's agents await new specializations, game modes, raids and much more! The adventure will take place in a fractured Washington D.C post-apocalyptic tint.",
+        price: 9.66,
+        onsale: 69.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 43,
+        title: "Watch Dogs 2",
+        src: Game43,
+        content: "Watch Dogs 2 is a sequel to Ubisoft's first open-world hacker-vigilante simulator Watch Dogs. The player becomes Marcus Holloway, a young man living in the sunny San Francisco, allied with DedSec to take down ctOS 2.0, the successor of the invasive operating system known from the previous game. As a third person shooter, Watch Dogs 2 offers a variety of options for both non-lethal and lethal resolutions of challenges offered to the players.",
+        price: 12.08,
+        onsale: 69.99,
+        new: "",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 44,
+        title: "Grand Theft Auto V",
+        src: Game44,
+        content: "One of the biggest action-adventure games awaits you with its huge world, interesting gameplay, rich story, and memorable characters. Take control over Franklin, Trevor, and Michael and make your dreams come true in San Andreas. Grand Theft Auto V is an action-adventure game where you can roam the open world as much as you wish and complete the story missions which additionally unlock gameplay mechanics.",
+        price: 26.47,
+        onsale: 0,
+        new: "new",
+        count: 1,
+        discount: 0
+    },
+    {
+        _id: 45,
+        title: "MARVEL'S AVENGERS",
+        src: Game45,
+        content: "In the game, you will take on challenges of restoring peace to the world through the powers of numerous famous Marvel heroes such as Captain America, Hulk, Black Widow, Iron Man, Thor and Ms. Marvel. With the progression of the story, new Super Heroes and regions to play in are unlocked. Additionally, during the gameplay, you will be able to meet other non-playable heroes which will help you on your way. Be aware though, as not only memorable heroes appear during the story.",
+        price: 30.02,
+        onsale: 69.99,
+        new: "",
+        count: 1,
+        discount: 0
+    }
+]
